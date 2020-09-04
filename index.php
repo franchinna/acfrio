497089e631c95371c8f8700a3addd55bc08b36b8
unknown <franqui>
2020-09-04 20:25:37 -0300

    se suben los archivos al repo. Carpeta admin, solo tiene un archivo para que la misma se cree. (git no te lee la carpeta si no hay archivos dentro)

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="res/bootstrap/css/bootstrap.css">
+    <title>acfrio - Aire Acondicionado para Automotor</title>
+</head>
+<body>
+    <header></header>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
