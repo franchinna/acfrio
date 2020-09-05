e446758b98c6695692d9b842c601bdaf5cee9dbe
unknown <franqui>
2020-09-04 21:44:53 -0300

    se suma el nav-bar en index.php, sumamos el logo con href a acfrio.com.ar y cambiamos el icono de menu hamburguesa por defecto

--- index.php
+++ index.php
@@ -6,11 +6,42 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="res/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="res/css/style.css">
     <title>acfrio - Aire Acondicionado para Automotor</title>
 </head>
 <body>
-    <header></header>
-    <main></main>
+    <header>
+        <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand" href="http://acfrio.com.ar"><h1>acfrio - aire acondicionado automotor</h1></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <p>Funciona bootstraps!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
     <footer></footer>
+    <script src="res/bootstrap/js/jquery-3.5.1.min.js"></script>
+    <script src="res/bootstrap/js/bootstrap.js"></script>
 </body>
 </html>
